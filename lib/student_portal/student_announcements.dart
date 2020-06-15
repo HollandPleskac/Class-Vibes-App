92445409fd264c73a592565f950cd257329cd0db
HollandPleskac <hollandpleskac@gmail.com>
2020-06-14 22:43:45 -0700

    meetings/announcements student

---
+++ lib/student_portal/student_announcements.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentAnnouncements extends StatefulWidget {
+  @override
+  _StudentAnnouncementsState createState() => _StudentAnnouncementsState();
+}
+
+class _StudentAnnouncementsState extends State<StudentAnnouncements> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/student_portal/student_meetings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentMeetings extends StatefulWidget {
+  @override
+  _StudentMeetingsState createState() => _StudentMeetingsState();
+}
+
+class _StudentMeetingsState extends State<StudentMeetings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
